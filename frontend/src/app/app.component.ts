5993b42428559dd316a77a6576235262be226c54
tomasz.kleiner@e2open.com <tomasz.kleiner@e2open.com>
2025-11-11 20:23:22 +0100

    Use static json file as data source (remove backend calls)

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, Inject, DOCUMENT } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
-import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -9,27 +8,6 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Parodioczolko';
-
-  constructor(@Inject(DOCUMENT) private document: Document) {}
-
-  ngOnInit() {
-    // Add preconnect to API domain for better performance
-    this.addApiPreconnect();
-  }
-
-  private addApiPreconnect() {
-    if (environment.apiUrl) {
-      try {
-        const apiUrl = new URL(environment.apiUrl);
-        const preconnectLink = this.document.createElement('link');
-        preconnectLink.rel = 'preconnect';
-        preconnectLink.href = apiUrl.origin;
-        this.document.head.appendChild(preconnectLink);
-      } catch (error) {
-        console.warn('Could not add API preconnect link:', error);
-      }
-    }
-  }
 }
--- frontend/src/app/models/song.model.ts
+++ frontend/src/app/models/song.model.ts
@@ -1,8 +1,6 @@
 export interface Song {
   id: string;
-  partitionKey: string;
   artist: string;
   name: string;
   year: number;
-  etag?: string;
 }
