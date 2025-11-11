1ec54df5aec545593271600b9b7d7ac0123fe0df
tomasz.kleiner@e2open.com <tomasz.kleiner@e2open.com>
2025-11-11 16:58:15 +0100

    Update tests

--- frontend/src/app/services/song.service.spec.ts
+++ frontend/src/app/services/song.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
-import { Song } from './song';
+import { SongService } from './song.service';
 
-describe('Song', () => {
-  let service: Song;
+describe('SongService', () => {
+  let service: SongService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(Song);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.inject(SongService);
   });
 
   it('should be created', () => {
